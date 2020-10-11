53dc356fc9392c8ef6cfb0f1bf97ad3bad4ecb46
Jou Hattori <j.h178bz@gmail.com>
2020-10-11 12:48:45 +0900

    workページの実装

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -3,7 +3,17 @@
     <h1 class="content-title">Work</h1>
     <div class="Work">
       <div class="work-content">
-
+        <div class="card card-skin">
+          <a href="https://street-racing-app.herokuapp.com/"><div class="card-imgframe"></div></a>
+          <div class="card-textbox">
+            <div class="card-titletext">
+              SR Share
+            </div>
+            <div class="card-overviewtext">
+              バイクタイヤに特化したレビュー共有SNSサービス
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
